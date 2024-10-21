9b962c91de712089ab9a10a5c157d9a826ae1279
Nikola <nikola.novakovic.21@singimail.rs>
2024-10-21 13:53:29 +0200

    list_and_details commite

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,32 +1,53 @@
-import {HttpClient} from '@angular/common/http';
-import {inject, Injectable} from '@angular/core';
-import {FlightModel} from "./models/flight.model";
-import {PageModel} from "./models/page.model";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { FlightModel } from '../models/flight.model';
+import { PageModel } from '../models/page.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebService {
 
+  private static instance: WebService
   private baseUrl: string
   private client: HttpClient
 
-  constructor() {
+  private constructor() {
     this.baseUrl = "https://flight.pequla.com/api"
     this.client = inject(HttpClient)
   }
 
-  public getRecommendedFlights() {
-    const url = `${this.baseUrl}/flight?page=0&size=3&sort=scheduledAt,desc&type=departure`
+  public static getInstance() {
+    if (this.instance == undefined)
+      this.instance = new WebService()
+    return this.instance
+  }
+
+  public getFlights(page = 0, size = 10, sort = "scheduledAt,desc") {
+    const url = `${this.baseUrl}/flight?page=${page}&size=${size}&sort=${sort}&type=departure`
     return this.client.get<PageModel<FlightModel>>(url)
+  }
+
+  public getRecommendedFlights() {
+    return this.getFlights(0, 3)
+  }
+
+  public getFlightById(id: number) {
+    const url = `${this.baseUrl}/flight/${id}`
+    return this.client.get<FlightModel>(url)
   }
 
   public getDestinationImage(dest: string) {
     return 'https://img.pequla.com/destination/' + dest.split(" ")[0].toLowerCase() + '.jpg'
   }
 
-  public getFlightById(id: number) {
-    const url = `${this.baseUrl}/flight/${id}`;
-    return this.client.get<FlightModel>(url);
+  public formatDate(iso: string | null) {
+    if (iso == null) return 'On Time'
+    return new Date(iso).toLocaleString('sr-RS')
+  }
+
+  public formatValue(str: string | null) {
+    if (str == null) return 'N/A'
+    return str
   }
 }
